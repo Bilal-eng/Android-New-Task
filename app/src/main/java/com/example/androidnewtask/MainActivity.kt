2d616163251185461ea96044369b6999c3928084
Bilal-eng <bilal.sheikh@consultant.turkcell.com.tr>
2021-03-22 00:26:16 +0300

    Setup Bottom Navigation with it's icons

--- app/src/main/java/com/example/androidnewtask/MainActivity.kt
+++ app/src/main/java/com/example/androidnewtask/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.androidnewtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.categoriesFragment,
+                R.id.vendorsFragment, R.id.wishlistsFragment, R.id.profileFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
