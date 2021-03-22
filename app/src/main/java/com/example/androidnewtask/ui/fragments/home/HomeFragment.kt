f3f72cc437c7767014f7e14e9312ce49e7e3716c
Bilal-eng <bilal.sheikh@consultant.turkcell.com.tr>
2021-03-22 14:13:13 +0300

    Create xml layout file of HomeFragment and Adapter layut items

--- app/src/main/java/com/example/androidnewtask/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidnewtask/ui/fragments/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.androidnewtask.ui.fragments
+package com.example.androidnewtask.ui.fragments.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.androidnewtask.R
 
-class VendorsFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vendors, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 }
